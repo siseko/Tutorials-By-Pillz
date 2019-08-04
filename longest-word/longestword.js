8d53536926c537d892cd655a5d698490e677b760
mpilo.dlamini@yahoo.com <mpilo.dlamini@yahoo.com>
2019-08-04 18:24:58 +0200

    Adding longest word algorithm no UI just print to console

---
+++ longest-word/longestword.js
@@ -0,0 +1,38 @@
+function sentenceCount(sentence){
+    return sentence.length;
+}
+
+console.log(sentenceCount("Life Thulile Fikasenzani Mpendulo Nicolas Nkhosi"));
+
+function findTheLongestWord(){
+
+var sentenceWithLongWord = "Life Thulile Fikasenzani Mpendulo Nicolas Nkhosi";
+
+var arrayedSentenceWithLongWord = sentenceWithLongWord.split(' ');
+
+console.log(arrayedSentenceWithLongWord);
+
+//Start the looping process
+
+var longestWordNumber = 0;
+var longestWordValue;
+
+for(var i = 0; i < arrayedSentenceWithLongWord.length; i++ ){
+    if(arrayedSentenceWithLongWord[i].length > longestWordNumber){ //checking the lonhestword counter variable
+        longestWordNumber = arrayedSentenceWithLongWord[i].length;
+        longestWordValue = arrayedSentenceWithLongWord[i];
+    }
+}
+    return [longestWordValue, longestWordNumber ];
+}
+
+function printMEaningFulSentence(){
+    var valuesArrray = findTheLongestWord();
+    var longestWordItself = valuesArrray[0];
+    var numberOfCharsInWord = valuesArrray[1];
+    var meaningfulSentence = "The longest word has is " + longestWordItself + "and it contains " + numberOfCharsInWord + " characters."
+    
+    console.log(meaningfulSentence);
+}
+printMEaningFulSentence();
+
