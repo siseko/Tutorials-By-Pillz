473f9758c81c7e7ee8e82d763e6fe09caf8d8995
mpilo.dlamini@yahoo.com <mpilo.dlamini@yahoo.com>
2019-08-04 17:02:01 +0200

    Fixed responsive view UI added respective functionality to buttons

--- calculator.js
+++ calculator.js
@@ -1,15 +1,33 @@
+// function greeting() {
+//     var name = 
+// }
+
+
 function add() {
-    alert("add");
+    var num1 = parseInt(document.getElementById("number1").value);
+    var num2 = parseInt(document.getElementById("number2").value);
+    sum = num1 + num2;
+    document.getElementById("sumAnswer").innerHTML = sum;
 }
 
 function multiply() {
-    alert("multiply");
+    var num1 = parseInt(document.getElementById("number1").value);
+    var num2 = parseInt(document.getElementById("number2").value);
+    product = num1 * num2;
+    document.getElementById("productAnswer").innerHTML = product;
 }
 
 function divide() {
-    alert("divide");
+    var num1 = parseInt(document.getElementById("number1").value);
+    var num2 = parseInt(document.getElementById("number2").value);
+    quotient = num1 / num2;
+    document.getElementById("quotientAnswer").innerHTML = quotient;
 }
 
+
 function subtract() {
-    alert("subtract");
+    var num1 = parseInt(document.getElementById("number1").value);
+    var num2 = parseInt(document.getElementById("number2").value);
+    difference = num1 - num2;
+    document.getElementById("differenceAnswer").innerHTML = difference;
 }
