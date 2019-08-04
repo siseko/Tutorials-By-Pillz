eb4a2594b99faaef80ec1505c8b3ff76ff7fb16b
Mpilo Pillz <mpilopillz@Mpilos-MacBook-Pro.local>
2019-08-04 09:48:40 +0200

    added style for a better looking form  and template java script

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function add() {
+    alert("add");
+}
+
+function multiply() {
+    alert("multiply");
+}
+
+function divide() {
+    alert("divide");
+}
+
+function subtract() {
+    alert("subtract");
+}
